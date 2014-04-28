b0dffbb64ad670738526288679dea19f063ad0d1
Ryan Taylor <ryanewtaylor@gmail.com>
2014-04-27 21:19:39 -0400

    Basic solution to Project Euler Problem 001 in F#

---
+++ problem-001/fsharp/Program.fs
@@ -0,0 +1,12 @@
+﻿let sumMultiplesOfThreeAndFive numbers = 
+    numbers
+    |> List.filter (fun x -> x % 3 = 0 || x % 5 = 0)
+    |> List.sum
+
+let sum = sumMultiplesOfThreeAndFive [0..999]
+
+printfn "The answer is: %i" sum
+printfn ""
+printfn "Hit any key to continue..."
+
+ignore (System.Console.ReadKey true)
