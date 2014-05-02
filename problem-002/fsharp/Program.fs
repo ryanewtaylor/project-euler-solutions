4b223e3664c4db7c290377ebe255374812504680
Ryan Taylor <ryanewtaylor@gmail.com>
2014-05-01 20:44:13 -0400

    Basic solution to Project Euler Problem 002 in F#

---
+++ problem-002/fsharp/Program.fs
@@ -0,0 +1,23 @@
+﻿let MakeFibonacciSequence max =
+    let rec FibonacciSeqImpl prev curr =
+        if prev + curr < max then
+            let current = prev + curr
+            let rest = FibonacciSeqImpl curr current
+            current :: rest
+        else
+            []
+
+    0::1::(FibonacciSeqImpl 0 1)
+
+let sumEvens list = 
+    list
+    |> List.filter (fun x -> x % 2 = 0)
+    |> List.sum
+
+let sum = sumEvens (MakeFibonacciSequence 4000000)
+
+printfn "The answer is: %i" sum
+printfn ""
+printfn "Hit any key to continue..."
+
+ignore (System.Console.ReadKey true)
